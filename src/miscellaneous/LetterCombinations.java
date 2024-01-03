663e7215811e1b1dfdab6650444d5c185fb171c9
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-01-03 07:10:57 +0530

    changes

---
+++ src/miscellaneous/LetterCombinations.java
@@ -0,0 +1,42 @@
+package miscellaneous;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LetterCombinations {
+
+	public static void main(String[] args) {
+		Map<Integer,String> dialMap=new HashMap<>();
+		dialMap.put(2, "abc");
+		dialMap.put(3, "def");
+		dialMap.put(4, "ghi");
+		dialMap.put(5, "jkl");
+		dialMap.put(6, "mno");
+		dialMap.put(7, "pqrs");
+		dialMap.put(8, "tuv");
+		dialMap.put(9, "wxyz");
+		
+		letterCombinations(" ","237",dialMap);
+	}
+	static void letterCombinations(String letterCombination,String digitString,Map<Integer, String> dialMap) {
+		
+		
+		if(digitString.trim().equals("")) {
+			System.out.print(letterCombination);
+			System.out.println();
+			return;
+		}
+		for(int j=0;j<digitString.length();j++) {
+		char activeDigit=digitString.charAt(j);
+		digitString=digitString.replace(activeDigit, ' ').trim();
+		String stringForDigit=dialMap.get(Integer.parseInt(String.valueOf(activeDigit)));
+		for(int i=0;i<stringForDigit.length();i++) {
+			char c=stringForDigit.charAt(i);
+			//letterCombination+=" "+c;
+			letterCombinations(letterCombination+c,digitString,dialMap);
+		}
+		}
+		
+	}
+
+}
