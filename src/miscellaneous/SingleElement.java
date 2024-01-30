b9279bd44747943516577ab4b2e134c051dea879
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-01-30 20:22:15 +0530

    Single Element

---
+++ src/miscellaneous/SingleElement.java
@@ -0,0 +1,54 @@
+package miscellaneous;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+public class SingleElement {
+
+    public static void main(String asd[]) throws Exception {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Long N = Long.parseLong(bufferedReader.readLine().trim());
+        long arr[] = new long[Math.toIntExact(N)];
+
+        arr = Stream.of(bufferedReader.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        System.out.println(binarySearchReturnsOnlySingleElement(arr));
+
+    }
+
+    static Long binarySearchReturnsOnlySingleElement(long arr[]) {
+        int low = 0, high = arr.length - 1, mid = 0, firstOccurrence = 0, secondOccurrence = 0;
+        if (arr.length == 1)
+            return arr[0];
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (mid == 0 && arr[mid + 1] != arr[mid])
+                return arr[mid];
+            else if (mid == arr.length - 1 && arr[mid - 1] != arr[mid])
+                return arr[mid];
+            else {
+                if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
+                    return arr[mid];
+                }
+                else {
+                    if (arr[mid] == arr[mid + 1]) {
+                        firstOccurrence = mid;
+                        secondOccurrence = mid + 1;
+
+                    }
+                    else if (arr[mid] == arr[mid - 1]) {
+                        firstOccurrence = mid - 1;
+                        secondOccurrence = mid;
+                    }
+                    if(firstOccurrence%2==0){
+                        low=mid+1;
+                    }else{
+                        high=mid-1;
+                    }
+                }
+            }
+        }
+        return -1l;
+    }
+}
