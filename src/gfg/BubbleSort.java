c21a16e33f7df38b578ea7c8811a776c37d16804
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-02-18 22:52:41 +0530

    Bubble Sort

---
+++ src/gfg/BubbleSort.java
@@ -0,0 +1,23 @@
+package gfg;
+
+public class BubbleSort {
+    public static void bubbleSort(int a[], int n)
+    {
+        boolean done=false;
+        while(!done){
+            done=true;
+            int i=0;
+            while(i<n-1){
+                if(a[i]>a[i+1]){
+                    int temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+                    done=false;
+                }i++;
+            }
+        }
+    }
+    public static void main(String asd[]){
+
+    }
+}
