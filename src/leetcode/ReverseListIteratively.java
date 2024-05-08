cee95a04925ed4e49d2af5db0d253b0e48a9da2e
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-05-08 09:07:04 +0530

    Reverse LinkedList Recursively

--- src/leetcode/ReverseListIteratively.java
+++ src/leetcode/ReverseListIteratively.java
@@ -1,2 +1,45 @@
-package leetcode;public class ReverseListIteratively {
+package leetcode;
+
+public class ReverseListIteratively {
+    public static ListNode reverseList(ListNode head) {
+      if(null==head){
+         return head;
+      }
+     ListNode prev=null;
+     ListNode current=head;
+     while(null!=current){
+         ListNode temp=current.next;
+         current.next=prev;
+         prev=current;
+         current=temp;
+     }
+     head=prev;
+     return head;
+    }
+    public static ListNode reverseListRecursively(ListNode head) {
+        if(null==head.next||null==head){
+            return head;
+        }
+        ListNode temp=head.next;
+        head.next=null;
+        ListNode p=reverseListRecursively(temp);
+        temp.next=head;
+        return p;
+    }
+    public static void main(String args[]){
+
+        ListNode head = new ListNode(1, null);
+        ListNode temp=head;
+        int counter=2;
+        while(counter!=3) {
+            ListNode node = new ListNode(2, null);
+            temp.next=node;
+            temp=node;
+            counter++;
+        }
+        System.out.println(head);
+        System.out.println(reverseList(head));
+
+    }
+
 }
