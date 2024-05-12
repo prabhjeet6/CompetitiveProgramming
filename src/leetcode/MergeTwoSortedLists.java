bf5cbb906fc762e4e2ff6cc3b4216d2e58d8a9b3
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-05-12 07:13:32 +0530

    MergeSortedLists

--- src/leetcode/MergeTwoSortedLists.java
+++ src/leetcode/MergeTwoSortedLists.java
@@ -5,8 +5,30 @@ public class MergeTwoSortedLists {
 
 
     }
-
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (null == list1) {
+            return list2;
+        }
+        if (null == list2) {
+            return list1;
+        }
+        ListNode head = null;
+        //ListNode temp=null;
+        ListNode tail = head;
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+            tail = head;
+        } else {
+            head = list2;
+            list2 = list2.next;
+            tail = head;
+        }
+       ListNode mergedList=mergeTwoLists(list1,list2);
+       head.next=mergedList;
+       return head;
+    }
+    public ListNode mergeTwoListsIteratively(ListNode list1, ListNode list2) {
         if (null == list1) {
             return list2;
         }
