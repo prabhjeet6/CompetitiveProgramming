c01bc8d7246b04f2c2d3cd29844cf5f63a2f778b
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-06-04 23:12:56 +0530

    PermutationSeq

---
+++ src/leetcode/Permutations.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack( nums, 0,  ans);
+        return ans;
+    }
+
+    private void backtrack(int[] nums, int i, List<List<Integer>> ans) {
+        if(i==nums.length){
+            List<Integer> subList=new ArrayList<>();
+            for(int val:nums){
+                subList.add(val);
+            }
+            ans.add(subList);
+            return;
+        }
+        for(int j=i;j<nums.length;j++ ){
+            //Input: nums = [1,2,3]
+            //Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+            swap(nums,i,j);
+            backtrack(nums,i+1,ans);
+            swap(nums,i,j);
+        }
+    }
+
+    //
+    void swap(int []nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
