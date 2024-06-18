11be090d00e340552ea98d4aaa7b5db112d8b04f
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-06-18 07:41:54 +0530

    NumberOfPaths

--- src/gfg/NumberOfPaths.java
+++ src/gfg/NumberOfPaths.java
@@ -12,3 +12,20 @@ public class NumberOfPaths{
         System.out.print(numberOfPaths(3,3));
     }
 }
+//Method2
+//Instead of making count the part of recursion, as an argument, use count as class variable, that way, its value will not vanish
+class NumberOfPaths2{
+    static int cnt=0;
+    static long numberOfPaths(int m, int n) {
+        countAllPaths(0,0,m,n);
+        return cnt;
+    }
+    static void countAllPaths(int i,int j,int m,int n){
+        if(i==m-1||j==n-1){
+            cnt++;
+            return;
+        }
+        countAllPaths(i,j+1,m,n);
+        countAllPaths(i+1,j,m,n);
+    }
+}
