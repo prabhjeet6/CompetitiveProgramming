b1dfa9de8b524079b2fe3ae9ce7cf61408f642fb
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-06-19 21:51:57 +0530

    codes

--- src/leetcode/FastExponentiation.java
+++ src/leetcode/FastExponentiation.java
@@ -5,21 +5,19 @@ public class FastExponentiation {
     public static void main(String args[]){
         System.out.println(new FastExponentiation().myPow(34.00515d,-3));
     }
-    /***public static double myPow(double x, int n) {
-        if(n==0)
-            return 1d;
-        if(n<0)                   //If n is negative
-            return myPow(1/x, -n);
-        double p=myPow(x,n/2);
-        if(n%2==0)
-            return p*p;
-       else{
-          // if(n==-1)
-           //    return 1/p*1/p*1/x;
-           return p*p*x;
+    //iterative Fast Exponentiation
+    static long power(long x, long y) {
+        //iterative Fast Exponentiation
+        long res = 1;
+        //bitwise & identifies if the right most bit is 1, suggesting number is odd
+        while (y > 0) {
+            //after multiplying the number once, power decreases by 1 and  becomes even(line 16 and 17)
+            if ((y & 1) == 1) res = (res * x) ;
+            y = y >> 1;
+            x = (x * x);
         }
-
-    }**/
+        return res;
+    }
 //ref: https://leetcode.com/problems/powx-n/solutions/1841398/java-100-recursive-solution-divide-conquer/
     public   double myPow(double x, int n) {
         return pow(x,n);
