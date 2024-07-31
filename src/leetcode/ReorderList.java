d3c4316fc4da41a646c30d020e902f759a25d827
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-07-31 10:58:59 +0530

    ReorderList

--- src/leetcode/ReorderList.java
+++ src/leetcode/ReorderList.java
@@ -28,34 +28,34 @@ class ReorderList {
 
 
     public void reorderList(ListNode head) {
-        ListNode temp=head;
-       while(null!=temp.next){
-           temp=temp.next;
-       }
-       ListNode tail=temp;
-       recurse(head,tail);
+        ListNode temp = head;
+        while (null != temp.next) {
+            temp = temp.next;
+        }
+        ListNode tail = temp;
+        recurse(head, tail);
     }
 
     private void recurse(ListNode head, ListNode tail) {
-       if(null==head){
-           return;
-       }
-       ListNode nextHead=head.next;
-       head.next=tail;
-       ListNode nextItr=nextHead;
+        if (null == head) {
+            return;
+        }
+        ListNode nextHead = head.next;
+        head.next = tail;
+        ListNode nextItr = nextHead;
         ListNode nextTail = null;
-       while(null!=nextItr&&null!=nextItr.next){
+        while (null != nextItr && null != nextItr.next) {
 
-           if(null==nextItr.next.next){
-               nextTail=nextItr;
-               nextTail.next=null;
-           }
-           nextItr=nextItr.next;
-       }
-       if(null!=tail) {
-           tail.next = nextHead;
-       }
-       recurse(nextHead,nextTail);
+            if (null == nextItr.next.next) {
+                nextTail = nextItr;
+                nextTail.next = null;
+            }
+            nextItr = nextItr.next;
+        }
+        if (null != tail) {
+            tail.next = nextHead;
+        }
+        recurse(nextHead, nextTail);
 
     }
 
