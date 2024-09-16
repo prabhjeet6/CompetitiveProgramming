9d179c5d40f8e0b47e0541115da05c7534be3530
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-09-16 17:08:46 +0530

    left and right view of binary tree iterative

--- src/gfg/LeftViewOfBinaryTree.java
+++ src/gfg/LeftViewOfBinaryTree.java
@@ -3,6 +3,8 @@ package gfg;
 //https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class LeftViewOfBinaryTree {
     int maxLevelSoFar = -1;
@@ -12,7 +14,6 @@ class LeftViewOfBinaryTree {
     ArrayList<Integer> leftView(Node root) {
         solve(root, 0);
         return res;
-
     }
 
     void solve(Node root, int level) {
@@ -25,6 +26,36 @@ class LeftViewOfBinaryTree {
         }
         solve(root.left, level + 1);
         solve(root.right, level + 1);
+    }
 
+    ArrayList<Integer> leftViewIteratively(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        queue.add(root);
+        boolean isLevelFirstElementAdded=false;
+        while (!queue.isEmpty()) {
+            //queueSize does not determine the actual queue size, rather it determines,
+            // size of queue at the start of a level
+            int queueSize=queue.size();
+            while(queueSize>0) {
+                Node current = queue.remove();
+                if(!isLevelFirstElementAdded) {
+                    res.add(current.data);
+                    isLevelFirstElementAdded=true;
+                }
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+                queueSize--;
+            }
+            isLevelFirstElementAdded=false;
+        }
+        return res;
     }
 }
--- src/gfg/RightViewOfBinaryTree.java
+++ src/gfg/RightViewOfBinaryTree.java
@@ -2,6 +2,8 @@ package gfg;
 //https://www.geeksforgeeks.org/problems/right-view-of-binary-tree/1
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class RightViewOfBinaryTree {
     int maxLevelSoFar = -1;
@@ -26,5 +28,37 @@ class RightViewOfBinaryTree {
         solve(root.left, level + 1);
 
 
+    }
+    ArrayList<Integer>  rightViewIteratively(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        queue.add(root);
+        boolean isLevelFirstElementAdded=false;
+        while (!queue.isEmpty()) {
+            //queueSize does not determine the actual queue size, rather it determines,
+            // size of queue at the start of a level
+            int queueSize=queue.size();
+            while(queueSize>0) {
+                Node current = queue.remove();
+                if(!isLevelFirstElementAdded) {
+                    res.add(current.data);
+                    isLevelFirstElementAdded=true;
+                }
+                //For right view, instead of the left child, we check for right child first,
+                //in case, right child is not present, we look for left child
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                queueSize--;
+            }
+            isLevelFirstElementAdded=false;
+        }
+        return res;
     }
 }
