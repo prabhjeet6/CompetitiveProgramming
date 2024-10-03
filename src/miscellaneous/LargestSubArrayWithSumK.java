e7366e2d299fee34c25144de1fe51fcfb20d1404
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-10-03 15:46:33 +0530

    LargestSubArrayWithSumK

--- src/miscellaneous/LargestSubArrayWithSumK.java
+++ src/miscellaneous/LargestSubArrayWithSumK.java
@@ -1,2 +1,44 @@
-package miscellaneous;public class LargestSubArrayWithSumK {
+package miscellaneous;/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class LargestSubArrayWithSumK {
+    public static void main(String[] args) throws java.lang.Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bufferedReader.readLine());
+        while (T > 0) {
+            T--;
+            String[] input = bufferedReader.readLine().split(" ");
+            int N = Integer.parseInt(input[0]);
+            int k = Integer.parseInt(input[1]);
+            long[] arr = Arrays.stream(bufferedReader.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+            long[] prefixSum = new long[N];
+            prefixSum[0] = arr[0];
+            for (int i = 1; i < N; i++) {
+                prefixSum[i] = arr[i] + prefixSum[i - 1];
+            }
+            Map<Long, Long> map = new HashMap<>();
+            map.put(0L, -1L);
+            boolean found = false;
+            long maxSubArrayLength = -1L;
+            for (int i = 0; i < N; i++) {
+                if (map.containsKey(prefixSum[i] - k)) {
+                    found = true;
+                    maxSubArrayLength = Math.max(maxSubArrayLength, i - map.get(prefixSum[i] - k));
+                } else {
+                    map.put(prefixSum[i], (long) i);
+                }
+            }
+            if (!found) {
+                System.out.println(0);
+            } else {
+                System.out.println(maxSubArrayLength);
+            }
+        }
+        bufferedReader.close();
+
+    }
 }
