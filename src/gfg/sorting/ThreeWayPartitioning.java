94d5c8444ca5d1edcfde902f54f51a4e2a328060
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-11-16 20:05:17 +0530

    ThreeWayPartitioning

---
+++ src/gfg/sorting/ThreeWayPartitioning.java
@@ -0,0 +1,52 @@
+package gfg.sorting;
+
+
+import java.util.Arrays;
+
+class ThreeWayPartitioning {
+
+
+    public static void main(String[] as) {
+        int[] a = new int[]{1, 2, 3, 3, 4};
+        threeWayPartition(a, 1, 2);
+        System.out.println(Arrays.toString(a));
+    }
+
+    //Function to partition the array around the range such
+    //that array is divided into three parts.
+    static void swap(int[] a, int x, int y) {
+        int temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
+
+    public static void threeWayPartition(int[] array, int a, int b) {
+
+        int lessThanA = 0, aToB = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < a) {
+                lessThanA++;
+            } else if (array[i] >= a && array[i] <= b) {
+                aToB++;
+            }
+        }
+        int moreThanBCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (!(array[i] > b)) {
+                if (i != moreThanBCount) {
+                    swap(array, i, moreThanBCount);
+                }
+                moreThanBCount++;
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < (lessThanA + aToB); i++) {
+            if (!(array[i] >= a && array[i] <= b)) {
+                if (i != count) {
+                    swap(array, i, count);
+                }
+                count++;
+            }
+        }
+    }
+}
